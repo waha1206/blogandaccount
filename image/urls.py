dc290c1a05f57bd545acd0eeada963994c136c8d
waha1206 <waha1206@gmail.com>
2020-06-09 04:33:57 +0800

    增加了image_list的分頁效果

--- image/urls.py
+++ image/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('upload/', views.image_upload, name='image_upload'),
 	path('list/', views.image_list, name='image_list'),
 	path('<int:id>/<str:slug>/', views.image_detail, name='image_detail'),
+	path('like/<int:id>', views.image_like, name='image_like'),
+	path('unliike/<int:id>', views.image_unlike, name='image_unlike'),
 ]
--- image/views.py
+++ image/views.py
@@ -1,10 +1,11 @@
 from urllib import request
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import ImageForm
 from .models import Image
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 @login_required
@@ -21,16 +22,37 @@ def image_upload(request):
 		form = ImageForm()
 	return render(request, 'image/image_upload.html', {'form':form})
 
-@login_required
-def image_list(request):
-	return render(request, 'image/image_list.html')
+# @login_required
+# def image_list(request):
+# 	return render(request, 'image/image_list.html')
 
 @login_required
 def image_list(request):
-	image_list = Image.objects.filter(user=request.user)
-	return render(request, 'image/image_list.html', {'images':image_list})
+	# image_list = Image.objects.filter(user=request.user)
+	images = Image.objects.all()
+	paginator = Paginator(images, 12)
+	page = request.GET.get('page')
+	try:
+		images = paginator.page(page)
+	except PageNotAnInteger:
+		images = paginator.page(1)
+	except EmptyPage:
+		images = paginator.page(paginator.num_pages)
+	return render(request, 'image/image_list.html', {'images':images})
 
 @login_required
 def image_detail(request, id, slug):
 	image = Image.objects.filter(id=id, slug=slug).first()
 	return render(request, 'image/image_detail.html', {'image':image})
+
+@login_required
+def image_like(request, id):
+	image = Image.objects.get(id=id)
+	image.user_like.add(request.user)
+	return redirect('image:image_detail', id=id, slug=image.slug)
+
+@login_required
+def image_unlike(request, id):
+	image = Image.objects.get(id=id)
+	image.user_like.remove(request.user)
+	return redirect('image:image_detail', id=id, slug=image.slug)
