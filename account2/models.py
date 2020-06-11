381b843696509db4f5b82366c2533637dff5372e
waha1206 <waha1206@gmail.com>
2020-06-11 13:08:11 +0800

    增加add_to_class

--- account2/models.py
+++ account2/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 from django.contrib.auth.models import User
@@ -15,3 +16,9 @@ class Contact(models.Model):
 		return '{} 跟隨 (關注) {}'.format(self.user_from, self.user_to)
 
 
+# 增加一個字段
+# 'self' 可以用 to=User 取代
+# through 意思是與哪個 class 產生關聯  例如 througth=Contact
+# synnetrical=多對多之間的關係不一定是對等的(非對稱關係)，你關聯我，但是我並不一定要關聯你，這裡指的是 User 與 Contact之間的關係
+# 非數據庫裡的字段
+User.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False))
